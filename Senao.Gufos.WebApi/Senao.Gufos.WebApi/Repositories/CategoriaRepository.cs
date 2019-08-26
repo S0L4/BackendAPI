082bfa9b7d0944af9c6e3e2339c91bee21230c80
Tiago Coelho Sola <46309932829@info.local>
2019-08-26 17:28:00 -0300

    Up

---
+++ Senao.Gufos.WebApi/Senao.Gufos.WebApi/Repositories/CategoriaRepository.cs
@@ -0,0 +1,65 @@
+﻿using Senao.Gufos.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senao.Gufos.WebApi.Repositories
+{
+    public class CategoriaRepository
+    {
+        public List<Categorias> Listar()
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // facilitar a minha vida
+
+                // select 
+                return ctx.Categorias.ToList();
+            }
+        }
+        
+        public void Cadastrar(Categorias categorias)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // insert
+                ctx.Categorias.Add(categorias);
+                ctx.SaveChanges();
+            }
+        }
+
+
+        public Categorias BuscarPorId(int id)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // buscar
+                return ctx.Categorias.FirstOrDefault(x => x.IdCategoria == id);
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // deletar 
+                Categorias Categoria = ctx.Categorias.Find(id);
+                ctx.Categorias.Remove(Categoria);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Categorias categoria)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // atualizar                                                  
+                Categorias CategoriaBuscada = ctx.Categorias.FirstOrDefault(x => x.IdCategoria == categoria.IdCategoria);
+                CategoriaBuscada.Nome = categoria.Nome;
+                ctx.Categorias.Update(CategoriaBuscada);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
